4bbc86c54cfee798785944031d4f18285ade9aa9
tojawawel <bgawel93@gmail.com>
2019-03-14 23:11:51 +0100

    add validation for blank brackets

--- app/validators/title_brackets_validator.rb
+++ app/validators/title_brackets_validator.rb
@@ -1,7 +1,11 @@
 class TitleBracketsValidator < ActiveModel::Validator
   def validate(record)
-    unless record.title == "The  the Ring [Lord of The Rings] (2001) {Peter Jackson}"
-      record.errors[:name] << 'namee is wrong'
-    end
+    record.errors[:title] << 'Brackets are blank!' if
+    blank_brackets?(record)
+  end
+
+  def blank_brackets?(record)
+    brackets_pairs = %w[ [] {} () ]
+    brackets_pairs.any? { |bracket_pair| record.title.include?(bracket_pair) }
   end
 end
