497f2dfe8d2719f5d907cab97efa8ee0eac333a8
tojawawel <bgawel93@gmail.com>
2019-03-15 08:58:34 +0100

    add validations for opened brackets

--- app/validators/title_brackets_validator.rb
+++ app/validators/title_brackets_validator.rb
@@ -1,11 +1,32 @@
 class TitleBracketsValidator < ActiveModel::Validator
   def validate(record)
-    record.errors[:title] << 'Brackets are blank!' if
-    blank_brackets?(record)
+    if blank_brackets?(record)
+      record.errors[:title] << 'Brackets are blank!'
+    elsif open_brackets?(record)
+      record.errors[:title] << 'Brackets are not closed properly!'
+    end
   end
 
+private
   def blank_brackets?(record)
     brackets_pairs = %w[ [] {} () ]
     brackets_pairs.any? { |bracket_pair| record.title.include?(bracket_pair) }
+  end
+
+  def open_brackets?(record)
+    pairs = {"{"=> "}", "["=> "]", "("=> ")"}
+    stack = []
+    record.title.each_char do |char|
+      if pairs.keys.any? { |opening_bracket| opening_bracket == char}
+        stack << char
+      elsif pairs.values.any? { |closing_bracket| closing_bracket == char}
+        if char != pairs[stack.last]
+          return true
+        else
+          stack.pop
+        end
+      end
+    end
+    stack.any?
   end
 end
