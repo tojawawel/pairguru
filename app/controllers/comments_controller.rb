f67e0e6840fdbf9a3b62fc7a158b53ca21feaf69
tojawawel <bgawel93@gmail.com>
2019-03-16 13:34:20 +0100

    code refactor and layout update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -22,7 +22,7 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    comment_params = params.require(:comment).permit(:body, :movie_id)
+    params.require(:comment).permit(:body)
   end
 
   def set_comment
